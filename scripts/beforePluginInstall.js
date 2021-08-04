1b4bad49a4a5473bfb4da2d34bda3e138c604707
Radhakrishnan Asokan <74190299+radhakrishnan-wavemaker@users.noreply.github.com>
2021-08-04 16:36:47 +0530

    Removed logs

--- scripts/beforePluginInstall.js
+++ scripts/beforePluginInstall.js
@@ -22,7 +22,6 @@ module.exports = function(context) {
             googleIndex = contents.indexOf('google()', allprojectsIndex);
 
         if ((googleIndex < 0 || googleIndex > jcenterIndex) && jcenterIndex !== -1 && googleIndex !== -1) {
-            console.warn("inside the google block");
             contents = contents.slice(0, jcenterIndex) + "google()\n\t\t" + contents.slice(jcenterIndex);
             fs.writeFileSync(buildGradlePath, contents, 'utf8');
             console.log("Updated build.gradle with google repo as before to jcenter");
