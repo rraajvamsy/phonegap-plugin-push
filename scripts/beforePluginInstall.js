e77d64340da5dfa3e73f5efec23cb1de03bd51f0
Radhakrishnan Asokan <74190299+radhakrishnan-wavemaker@users.noreply.github.com>
2021-08-04 16:26:17 +0530

    Update beforePluginInstall.js

--- scripts/beforePluginInstall.js
+++ scripts/beforePluginInstall.js
@@ -25,7 +25,7 @@ module.exports = function(context) {
             console.warn(jcenterIndex);
             console.warn(googleIndex);
 
-        if (googleIndex < 0 || googleIndex > jcenterIndex && jcenterIndex !== -1 && googleIndex !== -1) {
+        if ((googleIndex < 0 || googleIndex > jcenterIndex) && jcenterIndex !== -1 && googleIndex !== -1) {
             console.warn("inside the google block");
             contents = contents.slice(0, jcenterIndex) + "google()\n\t\t" + contents.slice(jcenterIndex);
             fs.writeFileSync(buildGradlePath, contents, 'utf8');
